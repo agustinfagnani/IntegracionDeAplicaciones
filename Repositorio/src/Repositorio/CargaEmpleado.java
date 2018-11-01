0f61a7e07d34c4df7b30018f783b140e08ce483f
Unknown <gastons315@gmail.com>
2018-11-01 12:05:45 -0300

    ModificaciónPresencia

--- Repositorio/src/Repositorio/CargaEmpleado.java
+++ Repositorio/src/Repositorio/CargaEmpleado.java
@@ -8,6 +8,6 @@ import java.rmi.RemoteException;
 public interface CargaEmpleado extends Remote {
 
 	//metodos con DTOs
-	public void cargaEmpleado(String CUITEmpresa, String legajo,String nombre, String apellido, String mail, String dni, String telefono) throws RemoteException;
+	public void cargaEmpleado(String CUITEmpresa, String legajo,String nombre, String apellido, String mail, String dni, String telefono, String tipo, int cantidadHorasMensual ) throws RemoteException;
 
 }
--- Servidor/src/integracion/PostPresencia.java
+++ Servidor/src/integracion/PostPresencia.java
@@ -6,31 +6,31 @@ import exception.ErrorConeccionPresenciaException;
 import negocio.Empleado;
 
 public class PostPresencia {
-	
+
 	private String IP = "192.168.157.150";
 	private final String miCuit = "30715087738";
-    
+
 	CargaEmpleado carga;
-	
-    public boolean getStub() {    	
-    		try {
+
+	public boolean getStub() {    	
+		try {
 			carga = (CargaEmpleado)Naming.lookup ("//" + IP + "/CargaEmpleado");
 			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
-    }
-    
-    public PostPresencia(Empleado em) throws ErrorConeccionPresenciaException{
-    	if(getStub()) 
-    	{
-    		try {
-				carga.cargaEmpleado(miCuit, "" + em.getLegajo(), em.getNombre(), em.getApellido(), em.getMail(), "" + em.getDNI(), em.getTelefono());
+	}
+
+	public PostPresencia(Empleado em) throws ErrorConeccionPresenciaException{
+		if(getStub()) 
+		{
+			try {
+				carga.cargaEmpleado(miCuit, "" + em.getLegajo(), em.getNombre(), em.getApellido(), em.getMail(), "" + Integer.toString(em.getDNI()), em.getTelefono(), "mensual", 160);
 			} catch (RemoteException e) {
 				throw new ErrorConeccionPresenciaException();
 			}    			
-    	} 
-    }
+		} 
+	}
 
 }
